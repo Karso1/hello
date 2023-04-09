e8548f8125ae5061a8e4b1cf93dae142d3338d06
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-04-09 10:12:57 +0800

    Add files via upload

---
+++ 1001helloworld/cmake-build-debug/CMakeFiles/1001helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1001helloworld.exe"
+  "1001helloworld.exe.manifest"
+  "1001helloworld.pdb"
+  "CMakeFiles/1001helloworld.dir/main.c.obj"
+  "lib1001helloworld.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1001helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

a905335646279ab920947dac3acd059c4b4b0648
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-04-10 20:34:51 +0800

    Add files via upload

---
+++ 1004cow/cmake-build-debug/CMakeFiles/1004cow.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1004cow.exe"
+  "1004cow.exe.manifest"
+  "1004cow.pdb"
+  "CMakeFiles/1004cow.dir/main.c.obj"
+  "lib1004cow.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1004cow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

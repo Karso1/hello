e7e5d7097a7c3a9fcb55de043e7b5b1821697595
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-04-11 20:26:11 +0800

    Add files via upload

---
+++ 411/cmake-build-debug/CMakeFiles/411.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "411.exe"
+  "411.exe.manifest"
+  "411.pdb"
+  "CMakeFiles/411.dir/main.c.obj"
+  "lib411.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/411.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

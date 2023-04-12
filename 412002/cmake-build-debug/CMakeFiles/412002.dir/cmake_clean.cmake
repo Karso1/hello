517c1cd45a307a38bcb1ddf954c4648557567f55
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-04-12 20:23:55 +0800

    Add files via upload

---
+++ 412002/cmake-build-debug/CMakeFiles/412002.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "412002.exe"
+  "412002.exe.manifest"
+  "412002.pdb"
+  "CMakeFiles/412002.dir/main.c.obj"
+  "lib412002.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/412002.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

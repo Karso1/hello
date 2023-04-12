bff5a91d7c391fbbfab56aad5eab7f129f564745
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-04-12 17:06:57 +0800

    Add files via upload

---
+++ 1007分段函数求值/cmake-build-debug/CMakeFiles/1007.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1007.exe"
+  "1007.exe.manifest"
+  "1007.pdb"
+  "CMakeFiles/1007.dir/main.c.obj"
+  "lib1007.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1007.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

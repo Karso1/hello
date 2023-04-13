627ee9a6e43b14bbd02638ae56b02a97614060d9
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-04-13 20:35:33 +0800

    Add files via upload

---
+++ 413/cmake-build-debug/CMakeFiles/413.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "413.exe"
+  "413.exe.manifest"
+  "413.pdb"
+  "CMakeFiles/413.dir/main.c.obj"
+  "lib413.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/413.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

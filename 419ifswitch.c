c24c6b5e6c00c154997f9994beb54a377130b56a
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-04-19 20:29:52 +0800

    Add files via upload

---
+++ 419ifswitch.c
@@ -0,0 +1,21 @@
+/*
+ * 表达式语句
+ * 函数调用语句
+ * 控制语句
+ * 复合语句
+ * 空语句
+ *
+ * if和switch是控制语句
+ * if和switch是选择结构，条件判断语句也叫分支语句
+ * 循环语句：do while for
+ * 转向语句：break，goto，continue，return
+ *
+ * switch（整型表达式）
+ * {
+ *     语句项；  /语句项就是一些case语句：：case  整型常量表达式：
+ *                                          语句；break；
+ *
+ *     default：语句；break；/输入格式出错，提示一下
+ * }
+ * switch允许嵌套使用
+ */

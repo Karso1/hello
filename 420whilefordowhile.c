f0caefdf49d581fd4435c26c981b71ea1d77e149
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-04-20 20:30:34 +0800

    Add files via upload

---
+++ 420whilefordowhile.c
@@ -0,0 +1,32 @@
+/*
+ * 循环语句while for do while
+ *
+ * while（表达式）
+ *      循环语句；
+ *
+ * break跳出循环
+ *
+ * continue；会跳过本次循环continue后的代码，执行下一次循环的判断
+ * while循环中的break是用于永久终止循环
+ * getchar读取了\n
+ *
+ *
+ * 通过scanf函数的%m格式控制可以指定输入域宽，（列数）  %4d，读取四位
+ * printf的%0可以控制输出数值的指定左面而不是用的空位置自动填0       %02d，输出占2列
+ *
+ * %.2f小数点保留两位
+ *
+ *
+ * for(i=1,i<=10,i++);
+ *
+ *
+ *
+ * do while循环
+ * do
+ * {
+ *      printf("%d",i);
+ *      i++;
+ *  }
+ *      while(i<=10);
+ *      return 0;
+ */

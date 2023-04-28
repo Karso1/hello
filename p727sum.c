8139b9c48486dcbc2edf94e035fb4c141efc92db
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-04-28 20:42:52 +0800

    Add files via upload

---
+++ p727sum.c
@@ -0,0 +1,13 @@
+/*
+ * 输入两个整数，输出和
+ */
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("please input data:");
+    scanf("%d %d",&a,&b);
+    printf("Result: %d + %d = %d",a,b,a+b);
+    return 0;
+}

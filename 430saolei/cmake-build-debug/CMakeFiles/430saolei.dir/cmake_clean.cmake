49d5da954ca97b1e890abb8479b2a8f8dd875721
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-04-30 22:40:50 +0800

    Add files via upload

---
+++ 430saolei/cmake-build-debug/CMakeFiles/430saolei.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "430saolei.exe"
+  "430saolei.exe.manifest"
+  "430saolei.pdb"
+  "CMakeFiles/430saolei.dir/430saolei.c.obj"
+  "CMakeFiles/430saolei.dir/game.c.obj"
+  "CMakeFiles/430saolei.dir/test.c.obj"
+  "lib430saolei.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/430saolei.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

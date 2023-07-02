018a115017081e9f0c45024e95bf04f9a2c53ff0
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-07-02 18:31:48 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,26 +1,32 @@
-//对称数
-//输入一个整型数，判断是否对称，如果是输出yes，否则输出no。不用考虑整型数过大，int类型存不下，不用考虑负值
-//例如12321是对称数，输出yes，1231不是，输出no
-//练习循环，并将整型数逆置，判断是否与最初值相等
+/*
+ * 求任意一个m*n矩阵的周边元素之和，2<=m<=20以及矩阵元素从键盘输入
+ *
+ */
 
 #include<stdio.h>
 int main()
 {
-    int a,b=0,backup_a;
-    scanf("%d",&a);//读取a
-    backup_a=a;//备份a的值
-    while(a)
+    int a1[20][20],i,j,m,sum=0;
+    printf("plz input m:");
+    scanf("%d",&m);
+    printf("plz input array:\n");
+    for(i=0;i<m-1;i++)
     {
-        printf("%d\n",a%10);
-        b=b*10+a%10;
-        a=a/10;
+        for(j=0;j<m-1;j++)
+        {
+            scanf("%d",&a1[i][j]);
+        }
     }
-    if(b==backup_a)
+    for(i=0;i<=m-1;i++)
     {
-        printf("yes\n");
-    }else
-    {
-        printf("no\n");
+        for(j=0;j<=m-1;j++)
+        {
+            if(i==0 || j==0 ||i==m-1||j==m-1)
+            {
+                sum = sum + a1[i][j];
+            }
+        }
     }
+    printf("sum=%d\n",sum);
     return 0;
 }

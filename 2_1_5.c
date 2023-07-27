1dbc651807f0c4bd0cb73a90f4d3381085f709ec
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-07-27 20:40:26 +0800

    Add files via upload

---
+++ 2_1_5.c
@@ -0,0 +1,18 @@
+/*
+ * 编写计算平方的公式
+ */
+/*
+#include<stdio.h>
+int pingfang(int x,int y)
+{
+    y = x*x;
+    return y;
+}
+int main()
+{
+    int a,b=0;
+    scanf("%d",&a);
+    b = pingfang(a,b);
+    printf("%d",b);
+    return 0;
+}*/

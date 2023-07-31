30aea16c2c4aee1888aecd5de760969a693f26a9
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-07-31 19:50:19 +0800

    Add files via upload

---
+++ 3_2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+
+        if (a>0)
+            printf("1");
+
+        else if(a=0)
+            printf("0");
+        else
+            printf("-1");
+    return 0;
+}

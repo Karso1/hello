bf3070f13d20a71ff9cf6aed8046e764a00c2d25
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-09-19 21:36:46 +0800

    Add files via upload

---
+++ e_44_1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+struct student
+{
+    long sno;
+    char name[10];
+    float score[3];
+};
+
+struct student fun(struct student a)
+{
+    int i;
+    a.sno = 10002;
+    strcpy(a.name,"LiSi");
+    for(i=0;i<3;i++)
+        a.score[i]+=1;
+    return a;
+}
+int main()
+{
+    struct student s ={10001,"ZhangSan",95,80,88},t;
+    int i;
+    printf("\nThe original data:\n");
+    printf("\nNo:%ld Name:%s\nScore:",s.sno,s.name);
+    for(i=0;i<3;i++)
+        printf("%6.2f",s.score[i]);
+    printf("\n");
+    t = fun(s);
+    printf("\nThe data after modigied:\n");
+    printf("\nNo:%ld Name:%s\nscore:",t.sno,t.name);
+    for(i=0;i<3;i++)
+        printf("%6.2f",t.score[i])  ;
+    printf("\n");
+    return 0;
+}

9b37c026d3181db85d703f866c2660f784a5d2f5
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-09-19 21:32:49 +0800

    Add files via upload

---
+++ e_40_1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+struct pair
+        {
+    int n1,n2;
+        };
+struct pair fun(int *c,int d) {
+    int i;
+    struct pair p;
+    p.n1 = p.n2 = 0;
+    for (i = 0; i < d; i++) {
+        if (c[i] % 2 == 0)
+            p.n1++;
+        else
+            p.n2++;
+    }
+    return p;
+}
+
+int main()
+{
+
+    int i,c[100],d;
+    struct pair p;
+    printf("plz input number :");
+    scanf("%d",&d);
+    printf("plz input %d",d);
+    for(i=0;i<d;i++)
+    scanf("%d",&(c[i]));
+
+    p = fun(c,d);
+    printf("n1 =%d,n2=%d",p.n1,p.n2);
+    return 0;
+}

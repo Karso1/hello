d44c1d1f00c766e00404b18f2209b10b5fd583b2
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-09-20 20:28:12 +0800

    Add files via upload

---
+++ e_46_3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void fun(int a,int b,long* c)
+{
+    *c=b/10*1000+a%10*100+b%10*10+a/10;
+}
+
+int main()
+{
+    int a,b;
+    long c;
+    printf("Input a b:\n");
+    scanf("%d%d",&a,&b);
+    fun(a,b,&c);
+    printf("The result is :%ld\n",c);
+    return 0;
+}

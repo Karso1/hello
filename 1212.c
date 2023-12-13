f9f2743a600f49cf48fc6050805c25abb5694274
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-12-13 11:57:06 +0800

    Add files via upload

---
+++ 1212.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct student
+{
+    int data;
+    char name[20];
+    double score;
+}Stu[3];
+int main()
+{
+    Stu[1] = {1,cahs,32};
+    return 0;
+}
